c8677b7a93999a516d30de9392106207726a64ae
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-09-23 18:03:45 -0700

    DBStorage-Amenity... and BOOM!

--- models/base_model.py
+++ models/base_model.py
@@ -17,16 +17,19 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
-            # Removed storage.new from here
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        # Removed storage.new from here
+        if kwargs:
+            if 'updated_at' in kwargs.keys():
+                kwargs['updated_at'] =
+                datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+
+            if 'created_at' in kwargs.keys():
+                kwargs['created_at'] =
+                datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if '__class__' in kwargs.keys():
+                del kwargs['__class__']
             # Loop through kwargs to create instance attribute(s)
             for key, value in kwargs.items():
                 setattr(self, key, value)
