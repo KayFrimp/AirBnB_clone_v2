cb897349548754df20b596e593cb14ad53fee184
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-10-20 11:49:11 -0700

    Improved engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -77,3 +77,7 @@ class DBStorage:
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """Closes the current session interacting with DB"""
+        self.__session.close()
