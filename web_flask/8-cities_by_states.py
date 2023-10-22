fb2fd29198ab0b4f4b64ce4b3fd3f2b34047158e
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-10-22 14:15:39 -0700

    Cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Flask Application Module"""
+
+from flask import Flask
+from models import storage
+from flask import render_template
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays list of cities by states in an HTML page"""
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
